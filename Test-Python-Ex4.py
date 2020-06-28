784dbef04678583926c68f7bd490c08fd4657f38
morgannvalero <67535315+morgannvalero@users.noreply.github.com>
2020-06-28 10:25:29 +0200

    Update Test-Python-Ex4.py

--- Test-Python-Ex4.py
+++ Test-Python-Ex4.py
@@ -11,19 +11,19 @@ Created on Sun Jun 28 09:42:15 2020
 import requests
 from bs4 import BeautifulSoup
 
-def scraping_twitter(url) :
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    follower = ""
-    ligne = 0
-    verif = False
-    while(verif == False) :
-        for i in soup.findAll('a'):
-            if(i['href'] == '/KMbappe/followers'):
-                follower = soup.findAll('a')[ligne]['title']
-                verif = True
-                ligne += 1
-    return int(follower.replace("&nbsp;", ""))
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+follower = ""
+ligne = 0
+verif = False
+while(verif == False) :
+    for i in soup.findAll('a'):
+        if(i['href'] == '/KMbappe/followers'):
+            follower = soup.findAll('a')[ligne]['title']
+            verif = True
+            ligne += 1
+print(int(follower.replace("&nbsp;", "")))
     
     
     
